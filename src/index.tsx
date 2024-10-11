100d92af1c73df69474cbc8169d0ce15177a641d
Ekaterina <militereva@mail.ru>
2024-10-11 15:45:25 +0300

    Add import store

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOMClient from 'react-dom/client';
+import App from './components/App/App';
+import { store } from './store/store';
+import { Provider } from 'react-redux';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const mountNode = document.getElementById('root') as HTMLElement;
+const root = ReactDOMClient.createRoot(mountNode);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store} >
+        <App />
+    </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
