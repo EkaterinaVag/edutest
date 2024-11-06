f25eb6fa7ef66998579cd4be054eb6dbcd0a8197
Ekaterina <militereva@mail.ru>
2024-11-06 20:39:46 +0300

    Add functionality to store answers array in localStorage

--- src/hooks/useSubmit.tsx
+++ src/hooks/useSubmit.tsx
@@ -1,22 +1,43 @@
-import React from "react";
+import { useState } from "react";
 import { useAppDispatch, useAppSelector } from "./hooks";
 import { nextQuestionIndex } from '../store/slices/questionSlice';
 import getCurrentQuestionIndex from '../store/slices/questionSelector';
+import { questions } from '../questions'
 
 interface FormValues {
     answer: string | string[],
+}
+
+interface UserAnswer {
+    questionId?: number
+    answer: string | string[]
 }
 
 const useSubmit = () => {
     const dispatch = useAppDispatch();
     const currentQuestionIndex = useAppSelector(getCurrentQuestionIndex);
 
+    const [usersAnswers, setUsersAnswers] = useState<UserAnswer[]>(() => {
+        const savedAnswers = localStorage.getItem('answers');
+        if (savedAnswers) {
+            return JSON.parse(savedAnswers);
+        }
+        return [];
+    });
+
     const handleSubmit = (values: FormValues) => {
+        const questionId = questions[currentQuestionIndex]?.id;
+        setUsersAnswers([
+            ...usersAnswers,
+            { questionId, answer: values.answer }
+        ]);
+        
+        localStorage.setItem('answers', JSON.stringify(usersAnswers));
         localStorage.setItem('currentQuestion', JSON.stringify(currentQuestionIndex + 1));
         dispatch(nextQuestionIndex());
     };
 
-    return { handleSubmit };
+    return { handleSubmit, usersAnswers };
 };
 
 export default useSubmit;
